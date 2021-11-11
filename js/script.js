e3f15eab922fdddb4f95d81586b65ca9b84310cc
KonevAndrew <a9139286138@yandex.ru>
2021-11-11 19:26:24 +0700

    дедалал карты, сделал адаптив и анимацию

--- js/script.js
+++ js/script.js
@@ -12,7 +12,8 @@ $(document).ready(function(){
                     }
             }
         ]
-  });  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  }); 
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass
